a1729749ecc400df71cf1c9e8d6c1a29f8478224
Marc Schwartz <marcschwartz1990@gmail.com>
2022-08-06 12:09:06 -0400

    Initial Commit

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Marc-Developer on 8/6/22.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
